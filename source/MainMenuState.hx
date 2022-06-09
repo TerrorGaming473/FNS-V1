1a8e4c1aa806fcd5398c905137382d024fecd1ea
Terror <89325375+TerrorGaming473@users.noreply.github.com>
2022-06-09 10:32:43 -0300

    Update MainMenuState.hx

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -149,7 +149,7 @@ class MainMenuState extends MusicBeatState
 		}
 		#end
 		FlxTween.tween(menuItem, { x:400 }, 1.5);
-		FlxTween.tween(BARmain, { x:480 }, 1.5);
+		FlxTween.tween(bar, { x:480 }, 1.5);
 
 		#if android
 		addVirtualPad(UP_DOWN, A_B_E);
